3fbefed16c046c39033d98cb0f88dc08ad9c1231
Manoj Rana <manoj.rana@rakuten.com>
2021-06-24 09:26:42 +0530

    SwiftUI App Travel INDIA

---
+++ Travel India/Travel India/Travel_IndiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Travel_IndiaApp.swift
+//  Travel India
+//
+//  Created by Rana, Manoj on 22/06/21.
+//
+
+import SwiftUI
+
+@main
+struct Travel_IndiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
